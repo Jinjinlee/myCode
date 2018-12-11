57d1f69a63c745206d4eb23d130de06a0c5a5bef
=Jinjinlee <=jinjinlee1990@gmail.com>
2018-12-11 10:31:55 -0500

    jobs app and running

--- Code/portfoliopro/jobs/models.py
+++ Code/portfoliopro/jobs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class jobs(models.Model):
-    image = model.ImageField(upload_to='images/')
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
